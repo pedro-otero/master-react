c8fd3e3caf85fb43a884f67fe797e31f26934411
Pedro Otero <pedro.otero.prada@gmail.com>
2017-12-04 21:26:37 -0500

    Layout

--- src/components/song.js
+++ src/components/song.js
@@ -6,17 +6,26 @@ class Song extends React.Component {
     render() {
         const {track, credits} = this.props;
         return <div>
-            {track && <div>
-                <p>{track.name}</p>
-                <p>{track.artists[0].name}</p>
-                <p>{track.album.name}</p>
+            {track && <div style={{display: 'flex', flexDirection: 'row'}}>
+                <div style={{flex: 3}}>
+                    <img alt="cover" src={track.album.images[0].url} style={{maxWidth: '100%'}}/>
+                </div>
+                <div style={{flexDirection: 'column', flex: 7}}>
+                    <p><strong>{track.name}</strong></p>
+                    <p>{track.artists[0].name}</p>
+                    <p>{track.album.name}</p>
+                </div>
             </div>}
             {credits && <div>
-                <p>{credits.composers.join(', ')}</p>
-                <p>{credits.producers.join(', ')}</p>
-                {credits.collaborators.map(collaborator => (
-                    <div>{collaborator.name}: {collaborator.roles.join(', ')}</div>
-                ))}
+                <p><strong>Composers: </strong>{credits.composers.join(', ')}</p>
+                <p><strong>Producers: </strong>{credits.producers.join(', ')}</p>
+                <p><strong>Other credits: </strong>
+                    <ul>
+                        {credits.collaborators.map(collaborator => (
+                            <li key={collaborator.name}>{collaborator.name}: {collaborator.roles.join(', ')}</li>
+                        ))}
+                    </ul>
+                </p>
             </div>}
         </div>
     }
