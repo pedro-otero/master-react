d820d9825661f8059623e7eceb8d548fb5a6d1f5
Pedro Otero <pedro.otero.prada@gmail.com>
2018-01-20 21:20:37 -0500

    Add blurred header as background of album image and track title

--- src/components/song/song.js
+++ src/components/song/song.js
@@ -7,20 +7,52 @@ class Song extends React.Component {
     render() {
 
         const {track, credits, artist} = this.props;
+        const mainArtistName = (track && track.artists[0].name) || '';
+
+        const roles = name => {
+            const result = [];
+            if (name === mainArtistName) {
+                result.push('main-artist');
+            }
+            if (credits.composers.includes(name)) {
+                result.push('composer');
+            }
+            if (credits.producers.includes(name)) {
+                result.push('producer');
+            }
+            if (track.artists.splice(1).map(artist => artist.name).includes(name)) {
+                result.push('featured');
+            }
+            return result;
+        }
+
+        const collaborator = name => {
+            return <span className={['collaborator', ...roles(name)].join(' ')}>{name}</span>
+        }
+
+        const layers = (image) => ({
+            backgroundImage: `linear-gradient(rgba(0,0,0,0.35) 95%, black), 
+            url(${image.url})`
+        });
 
         return <article>
-            {track && <header>
-                <div className="header-imageblock">
-                    <img alt="cover" src={track.album.images[0].url} className="header-image"/>
+            {track && artist && <div className="header">
+                <div className="content">
+                    <div>
+                        <img className="albumCover" alt="cover" src={track.album.images[0].url}/>
+                    </div>
+                    <div>
+                        <h2>{track.name}</h2>
+                        <h3>{mainArtistName} - {track.album.name}</h3>
+                    </div>
                 </div>
-                <div className="header-title">
-                    <h2>{track.name}</h2>
-                    <h3>{track.artists[0].name} - {track.album.name}</h3>
+                <div className="background" style={layers(artist.images[0])}>
+
                 </div>
-            </header>}
+            </div>}
             {credits && <div>
-                <p><h4>Composers: </h4>{credits.composers.join(', ')}</p>
-                <p><h4>Producers: </h4>{credits.producers.join(', ')}</p>
+                <p><h4>Composers: </h4>{credits.composers.map(collaborator)}</p>
+                <p><h4>Producers: </h4>{credits.producers.map(collaborator)}</p>
                 <p><h4>Other credits: </h4>
                     <ul>
                         {credits.collaborators.map(collaborator => (
