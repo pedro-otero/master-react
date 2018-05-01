5318035c00876c7869990fce41f6601eedfe55e7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-01 11:54:50 -0400

    Externalize be domain env variable

--- src/api/backend.js
+++ src/api/backend.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 export default function Backend() {
   this.getCredits = (track, album) => new Promise(function (resolve, reject) {
     request
-      .get(`http://localhost:3001/data/album/${album.id}`)
+      .get(`${process.env.REACT_APP_BE_DOMAIN}/data/album/${album.id}`)
       .end((err, res) => {
         if (err) {
           console.error(err);
