fbc9f5ac30bb2eccf1104c0f30247ad0de7e48a6
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 11:10:11 -0400

    Refactor

--- src/actions/spotify.js
+++ src/actions/spotify.js
@@ -2,16 +2,17 @@ import * as types from './types';
 
 export const getCurrentPlayback = () => function (dispatch, getState, { spotifyApi, backend }) {
   return spotifyApi.getCurrentPlayback()
-    .then((playback) => {
-      dispatch({ type: types.LOAD_PLAYBACK_SUCCESS, track: playback.body.item });
-      spotifyApi.getAlbum(playback.body.item.album.id).then((album) => {
-        dispatch({ type: types.LOAD_ALBUM_SUCCESS, album: album.body });
+    .then(({ body: playback }) => {
+      dispatch({ type: types.LOAD_PLAYBACK_SUCCESS, track: playback.item });
+      spotifyApi.getAlbum(playback.item.album.id).then(({ body: album }) => {
+        dispatch({ type: types.LOAD_ALBUM_SUCCESS, album });
       });
-      spotifyApi.getArtist(playback.body.item.artists[0].id).then((artist) => {
-        dispatch({ type: types.LOAD_ARTIST_SUCCESS, artist: artist.body });
+      spotifyApi.getArtist(playback.item.artists[0].id).then(({ body: artist }) => {
+        dispatch({ type: types.LOAD_ARTIST_SUCCESS, artist });
       });
-      backend.getCredits(playback.body.item.album.id).then((credits) => {
-        dispatch({ type: types.LOAD_CREDITS_SUCCESS, credits: credits.bestMatch.tracks.find(t => t.id === playback.body.item.id) });
+      backend.getCredits(playback.item.album.id).then(({ bestMatch: { tracks } }) => {
+        const credits = tracks.find(t => t.id === playback.item.id);
+        dispatch({ type: types.LOAD_CREDITS_SUCCESS, credits });
       });
     });
 };
