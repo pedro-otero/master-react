72aab0d133d757b5a75c2923f638a86a79bdb078
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 15:35:27 -0400

    Remove dead code

--- src/util/index.js
+++
@@ -1,6 +0,0 @@
-export function millisToString(millis) {
-  const seconds = millis / 1000;
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.floor(seconds % 60);
-  return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
-}
