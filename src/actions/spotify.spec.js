ae7b72c007b10f78fe406018a16f213796641966
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 10:30:38 -0400

    Add backend call test

--- src/actions/spotify.spec.js
+++ src/actions/spotify.spec.js
@@ -4,36 +4,42 @@ describe('Spotify async actions', () => {
   describe('getCurrentPlayback', () => {
     const album = { id: 'A1' };
     const artist = { id: 'AR1' };
+    const track = {
+      id: 'T1',
+      album,
+      artists: [artist],
+    };
     const spotifyApi = {
-      getAlbum: jest.fn(() => Promise.resolve({ body: { album } })),
+      getAlbum: jest.fn(() => Promise.resolve({ body: album })),
       getArtist: jest.fn(() => Promise.resolve(artist)),
       getCurrentPlayback: jest.fn(() => Promise.resolve({
         body: {
-          item: {
-            id: 'T1',
-            album,
-            artists: [artist],
-          },
+          item: track,
         },
       })),
     };
+    const backend = {
+      getCredits: jest.fn(() => Promise.resolve({})),
+    };
     getCurrentPlayback()(jest.fn(), null, {
       spotifyApi,
-      backend: {
-        getCredits: jest.fn(() => Promise.resolve({})),
-      },
+      backend,
     });
 
-    it('calls getCurrentPlayback once', () => {
+    it('calls spotifyApi#getCurrentPlayback once', () => {
       expect(spotifyApi.getCurrentPlayback.mock.calls.length).toEqual(1);
     });
 
-    it('calls getAlbum', () => {
+    it('calls spotifyApi#getAlbum', () => {
       expect(spotifyApi.getAlbum.mock.calls).toEqual([['A1']]);
     });
 
-    it('calls getArtist', () => {
+    it('calls spotifyApi#getArtist', () => {
       expect(spotifyApi.getArtist.mock.calls).toEqual([['AR1']]);
+    });
+
+    it('calls backend#getCredits', () => {
+      expect(backend.getCredits.mock.calls).toEqual([[track, album]]);
     });
   });
 });
