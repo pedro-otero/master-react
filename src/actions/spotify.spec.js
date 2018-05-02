7ed60f38d23a55cd24df2a5ae76a338d8fb9d1f1
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 10:06:49 -0400

    Setup mock data

--- src/actions/spotify.spec.js
+++ src/actions/spotify.spec.js
@@ -2,7 +2,32 @@ import { getCurrentPlayback } from './spotify';
 
 describe('Spotify async actions', () => {
   describe('getCurrentPlayback', () => {
-    //getCurrentPlayback()(jest.fn(), null, { spotifyApi, backend });
-    //getCurrentPlayback()(jest.fn(), null, { spotifyApi, backend });
+    const album = { id: 'A1' };
+    const artist = { id: 'A1' };
+    const spotifyApi = {
+      getAlbum: jest.fn(() => Promise.resolve({ body: { album } })),
+      getArtist: jest.fn(() => Promise.resolve(artist)),
+      getCurrentPlayback: jest.fn(() => Promise.resolve({
+        body: {
+          item: {
+            id: 'T1',
+            album,
+            artists: [artist],
+          },
+        },
+      })),
+    };
+    getCurrentPlayback()(jest.fn(), null, {
+      spotifyApi,
+      backend: {
+        getCredits: jest.fn(() => Promise.resolve({})),
+      },
+    });
+
+    describe('calls to getCurrentPlayback', () => {
+      it('calls it once', () => {
+        expect(spotifyApi.getCurrentPlayback.mock.calls.length).toEqual(1);
+      });
+    });
   });
 });
