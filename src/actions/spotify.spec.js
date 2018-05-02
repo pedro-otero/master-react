43629008ae08a522e9dd41beb118caa4837adfb3
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 10:14:47 -0400

    Test calls to spotify api functions

--- src/actions/spotify.spec.js
+++ src/actions/spotify.spec.js
@@ -3,7 +3,7 @@ import { getCurrentPlayback } from './spotify';
 describe('Spotify async actions', () => {
   describe('getCurrentPlayback', () => {
     const album = { id: 'A1' };
-    const artist = { id: 'A1' };
+    const artist = { id: 'AR1' };
     const spotifyApi = {
       getAlbum: jest.fn(() => Promise.resolve({ body: { album } })),
       getArtist: jest.fn(() => Promise.resolve(artist)),
@@ -24,10 +24,16 @@ describe('Spotify async actions', () => {
       },
     });
 
-    describe('calls to getCurrentPlayback', () => {
-      it('calls it once', () => {
-        expect(spotifyApi.getCurrentPlayback.mock.calls.length).toEqual(1);
-      });
+    it('calls getCurrentPlayback once', () => {
+      expect(spotifyApi.getCurrentPlayback.mock.calls.length).toEqual(1);
+    });
+
+    it('calls getAlbum', () => {
+      expect(spotifyApi.getAlbum.mock.calls).toEqual([['A1']]);
+    });
+
+    it('calls getArtist', () => {
+      expect(spotifyApi.getArtist.mock.calls).toEqual([['AR1']]);
     });
   });
 });
