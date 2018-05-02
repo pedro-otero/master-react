c463835e53f48a0e3c649a6b04b1561cf54af2b9
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-02 09:43:44 -0400

    Add empty test

---
+++ src/actions/spotify.spec.js
@@ -0,0 +1,8 @@
+import { getCurrentPlayback } from './spotify';
+
+describe('Spotify async actions', () => {
+  describe('getCurrentPlayback', () => {
+    //getCurrentPlayback()(jest.fn(), null, { spotifyApi, backend });
+    //getCurrentPlayback()(jest.fn(), null, { spotifyApi, backend });
+  });
+});
