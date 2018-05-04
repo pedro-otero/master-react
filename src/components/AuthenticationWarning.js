53fc78f454f9b1cdebe448bdba7028932196a329
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-04 10:26:07 -0400

    Lint

--- src/components/AuthenticationWarning.js
+++ src/components/AuthenticationWarning.js
@@ -18,7 +18,10 @@ const AuthenticationWarning = ({ clientId, redirectUri, scopes }) => {
       <div className="jumbotron">
         <h1 className="display-3">Hey there!</h1>
         <p>You need to login to Spotify to use this app</p>
-        <input type="button" className="btn btn-success" onClick={() => window.location = url} value="Login"/>
+        <input
+          type="button"
+          className="btn btn-success"
+          onClick={() => { window.location = url; }} value="Login"/>
       </div>
     </div>
   );
