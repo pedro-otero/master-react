ba0c228fd0b40b621f796ae2ec4f0d7174a02567
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-06 19:11:52 -0400

    Refactor tests

--- src/api/spotify.spec.js
+++ src/api/spotify.spec.js
@@ -4,8 +4,8 @@ describe('Spotify module', () => {
   it('instantiates the passed SpotifyWebApi module', () => {
     global.localStorage = { getItem: jest.fn(() => 'fakeToken') };
     const webApi = jest.fn(() => ({ setAccessToken: jest.fn() }));
-    const factory = Spotify(webApi);
-    factory(1, 2);
+    const getSpotifyModule = Spotify(webApi);
+    getSpotifyModule(1, 2);
     expect(webApi.mock.instances.length).toEqual(1);
   });
 
@@ -15,8 +15,8 @@ describe('Spotify module', () => {
       setAccessToken: jest.fn(),
       getAlbum: jest.fn(() => Promise.resolve('OK')),
     }));
-    const factory = Spotify(webApi);
-    const api = factory(1, 2);
+    const getSpotifyModule = Spotify(webApi);
+    const api = getSpotifyModule(1, 2);
     api.getAlbum().then(value => expect(value).toEqual('OK'), () => expect(undefined));
   });
 
@@ -27,8 +27,8 @@ describe('Spotify module', () => {
       getAlbum: jest.fn(() => Promise.reject({ statusCode: 401 })),
     }));
     const location = { reload: jest.fn() };
-    const factory = Spotify(webApi, location);
-    const api = factory(1, 2);
+    const getSpotifyModule = Spotify(webApi, location);
+    const api = getSpotifyModule(1, 2);
     api.getAlbum().then(() => {
       expect(location.reload).toHaveBeenCalled();
       done();
