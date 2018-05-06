28647c10ce3b38e8417a125c0b45359a0432370a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-06 19:58:17 -0400

    Use user module

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 
-
 import SpotifyCustomApiFactory from './api/spotify';
 import Backend from './api/backend';
+import getUser from './user';
 
 const CustomSpotifyApi = SpotifyCustomApiFactory(SpotifyWebApi, window.location);
 
@@ -17,21 +17,11 @@ const spotifyApi = CustomSpotifyApi({
   clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
 });
 const backend = new Backend();
+const user = getUser();
 
-const token = localStorage.getItem('token');
-const expiry = localStorage.getItem('expiry');
-const now = new Date().getTime();
-const difference = now - expiry;
-if (token && difference <= 0) {
+if (user.isAuthenticated()) {
   ReactDOM.render(<App {...{ spotifyApi, backend }}/>, document.getElementById('root'));
   registerServiceWorker();
 } else {
-  window.location = `${process.env.REACT_APP_SPOTIFY_AUTHORIZE_URL}?${[
-    ['client_id', process.env.REACT_APP_SPOTIFY_CLIENT_ID],
-    ['response_type', 'token'],
-    ['redirect_uri', process.env.REACT_APP_SPOTIFY_REDIRECT_URI],
-    ['state', 'reactApp'],
-    ['scope', process.env.REACT_APP_SPOTIFY_SCOPES],
-    ['show_dialog', 'false'],
-  ].map(pair => `${pair[0]}=${pair[1]}`).join('&')}`;
+  window.location = user.getAuthUrl();
 }
