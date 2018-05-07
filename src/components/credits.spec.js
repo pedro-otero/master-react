1bac6d926e70d579222c94c377e434307434c1c7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-07 19:39:48 -0400

    Test empty list

--- src/components/credits.spec.js
+++ src/components/credits.spec.js
@@ -7,7 +7,9 @@ import Credits from './credits';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Credits component', () => {
-  const wrapper = shallow(<Credits />);
+  const wrapper = shallow(<Credits data={[]} />);
 
-  it('does not break');
+  it('has no Collaborators component', () => {
+    expect(wrapper.find('Collaborator')).toHaveLength(0);
+  });
 });
