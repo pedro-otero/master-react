b341c03c47af3d8cc6e5259fd53bb13e6e4fcc23
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-08 16:11:08 -0400

    Add test for optional image class

--- src/components/cover.js
+++ src/components/cover.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Cover = ({ album }) => (
+const Cover = ({ album, imageClass }) => (
   <div
-      className="image"
+      className={`image${imageClass ? ` ${imageClass}` : ''}`}
       style={{ backgroundImage: `url(${album.images[0].url})` }}>
     <span className="year">{album.release_date.substring(0, 4)}</span>
   </div>);
 
 Cover.propTypes = {
   album: PropTypes.object.isRequired,
+  imageClass: PropTypes.string,
 };
 
 export default Cover;
--- src/components/cover.spec.js
+++ src/components/cover.spec.js
@@ -12,6 +12,13 @@ describe('Cover', () => {
     expect(wrapper.find('div[className="image"]').length).toEqual(1);
   });
 
+  it('displays album cover with applied class for image', () => {
+    const wrapper = shallow(<Cover
+        album={{ images: [{ url: 'img.jpg' }], release_date: '2004' }}
+        imageClass="someClass" />);
+    expect(wrapper.find('div[className="image someClass"]').length).toEqual(1);
+  });
+
   it('displays year', () => {
     const wrapper = shallow(<Cover album={{ images: [{ url: 'img.jpg' }], release_date: '2004' }} />);
     expect(wrapper.find('span[className="year"]').length).toEqual(1);
