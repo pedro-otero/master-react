a0f965092b5136bbb031485969b933840fa66f09
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-08 18:18:28 -0400

    Use banner component

--- src/components/song.js
+++ src/components/song.js
@@ -8,6 +8,7 @@ import Credits from './credits';
 import Label from './label';
 import Cover from './cover';
 import JointList from './joint-list';
+import Banner from './banner';
 
 const Song = ({
   track,
@@ -32,49 +33,35 @@ const Song = ({
     return 'with-credits';
   })();
 
-  const layers = image => ({
-    backgroundImage: `linear-gradient(rgba(0,0,0,0.1) 65%, black), 
-            url(${image.url})`,
-  });
-
   return <article>
     {status === 'empty' && <LoadingCircle message="Loading data from Spotify..." />}
-    <div className="header">
-      <div className="content">
-        {track && album && <Cover
-            album={album}
-            imageClass="albumCover"
-            yearClass="albumYear" />}
-        <div>
-          {track && <span>
-            <Label
-                className="artistName"
-                value={track.artists[0].name} />
-            <Label
-                className="trackName"
-                value={track.name} />
-            </span>}
-          {bestMatch && <span>
-            <JointList
-                className="composers"
-                start="("
-                values={bestMatch.composers}
-                end=")" />
-            <br />
-            <JointList
-                className="producers"
-                start="["
-                values={bestMatch.producers}
-                end="]" />
-          </span>}
-        </div>
+    {track && bestMatch && <Banner
+        src={artist.images[0].url}
+        className="content">
+      <Cover
+          album={album}
+          imageClass="albumCover"
+          yearClass="albumYear" />
+      <div>
+        <Label
+            className="artistName"
+            value={track.artists[0].name} />
+        <Label
+            className="trackName"
+            value={track.name} />
+        <JointList
+            className="composers"
+            start="("
+            values={bestMatch.composers}
+            end=")" />
+        <br />
+        <JointList
+            className="producers"
+            start="["
+            values={bestMatch.producers}
+            end="]" />
       </div>
-      {artist && <div
-          className="artistImg"
-          style={layers(artist.images[0])}>
-
-        </div>}
-    </div>
+      </Banner>}
     {status === 'search-not-started' && <LoadingCircle message="Starting search..." />}
     {status === 'with-credits' && <Progress
         size="small"
