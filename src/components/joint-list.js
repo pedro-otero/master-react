401665a67373d6722281fb58a8daa11b3f7a35dc
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-08 16:59:09 -0400

    Rename

--- src/components/joint-list.js
+++ src/components/joint-list.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InlineList=({values})=>{
+const JointList=({values})=>{
   return(
     <span className="main">{values.join(', ')}</span>
   );
 };
 
-InlineList.propTypes = {
+JointList.propTypes = {
   values:PropTypes.array.isRequired,
 }
 
-export default InlineList;
+export default JointList;
--- src/components/joint-list.spec.js
+++ src/components/joint-list.spec.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import InlineList from './inline-list';
+import JointList from './joint-list';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Inline List', () => {
   it('renders list of values', () => {
-    const wrapper = shallow(<InlineList values={['one', 'two', 'three']} />);
+    const wrapper = shallow(<JointList values={['one', 'two', 'three']} />);
     expect(wrapper.find('span[className="main"]').text()).toEqual('one, two, three');
   });
 });
