da2dd5f433dbb4671b0ea4dca0a6c972299b81ab
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-08 19:11:12 -0400

    Lint and add test

--- src/components/progress.js
+++ src/components/progress.js
@@ -6,14 +6,16 @@ import './progress.css';
 const Progress = ({ size, value }) => (
   <div className={`progress ${size}-progress`}>
     <div className="progress-all"></div>
-    <div className="progress-done" style={{ width: `${value}%` }}></div>
+    <div
+        className="progress-done"
+        style={{ width: `${value}%` }}></div>
   </div>);
 
 Progress.propTypes = {
   size: PropTypes.oneOf(['big', 'small']).isRequired,
   value: (props, value) => {
     if (props[value] < 0 || props[value] > 100) {
-      return Error('Invalid');
+      return Error('"progress" prop out of range (0 <= progress <= 100)');
     }
   },
 };
