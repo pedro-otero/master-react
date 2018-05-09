fc6c582453bb7c5d076b6402caf9349bfe76cf0d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-09 10:56:56 -0400

    Lint

--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as Observable from 'rxjs';
 
 import App from './App';
-import * as Observable from "rxjs";
 
 Enzyme.configure({ adapter: new Adapter() });
 
