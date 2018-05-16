6c04842c6366266ab60f195a7eabe127676bdf05
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-16 09:57:38 -0400

    Reduce function signature

--- src/user.js
+++ src/user.js
@@ -1,15 +1,15 @@
 import SpotifyCustomApiFactory from './api/spotify';
 
-export default (ApiClass, location, history) => ({
+export default (ApiClass, window) => ({
   isAuthenticated: () => {
-    if (location.hash) {
-      const pairs = location.hash.substring(1).split('&').map(pair => pair.split('='));
+    if (window.location.hash) {
+      const pairs = window.location.hash.substring(1).split('&').map(pair => pair.split('='));
       const token = pairs.filter(pair => pair[0] === 'access_token')[0][1];
       const expiresIn = pairs.filter(pair => pair[0] === 'expires_in')[0][1];
       const expiry = new Date(Date.now() + (expiresIn * 1000)).toISOString();
       localStorage.setItem('token', token);
       localStorage.setItem('expiry', expiry);
-      history.pushState({}, '', '/');
+      window.history.pushState({}, '', '/');
     }
     const token = localStorage.getItem('token');
     const expiry = new Date(localStorage.getItem('expiry'));
@@ -20,15 +20,15 @@ export default (ApiClass, location, history) => ({
   getAuthUrl: () => `${process.env.REACT_APP_SPOTIFY_AUTHORIZE_URL}?${[
     ['client_id', process.env.REACT_APP_SPOTIFY_CLIENT_ID],
     ['response_type', 'token'],
-    ['redirect_uri', location.href],
+    ['redirect_uri', window.location.href],
     ['state', 'reactApp'],
     ['scope', process.env.REACT_APP_SPOTIFY_SCOPES],
     ['show_dialog', 'false'],
   ].map(pair => `${pair[0]}=${pair[1]}`).join('&')}`,
   getApi: () => {
-    const CustomApi = SpotifyCustomApiFactory(ApiClass, location);
+    const CustomApi = SpotifyCustomApiFactory(ApiClass, window.location);
     return CustomApi({
-      redirectUri: location.href,
+      redirectUri: window.location.href,
       clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
     });
   },
--- src/user.spec.js
+++ src/user.spec.js
@@ -1,39 +1,47 @@
 import getUser from './user';
 
 describe('Auth module', () => {
-  const emptyHashLocation = {
-    hash: '',
-    href: 'http://frontend.org',
+  const emptyHashWindow = {
+    location: {
+      hash: '',
+      href: 'http://frontend.org',
+    },
   };
 
   it('#isAuthenticated = false when there is a token but it expired', () => {
     global.localStorage = { getItem: jest.fn(key => ({ token: '', expiry: new Date(Date.now() - 1000).toISOString() })[key]) };
-    const user = getUser(null, emptyHashLocation);
+    const user = getUser(null, emptyHashWindow);
     expect(user.isAuthenticated()).toEqual(false);
   });
 
   it('#isAuthenticated = false when there is not a token nor expiry', () => {
     global.localStorage = { getItem: jest.fn() };
-    const user = getUser(null, emptyHashLocation);
+    const user = getUser(null, emptyHashWindow);
     expect(user.isAuthenticated()).toEqual(false);
   });
 
   it('#isAuthenticated = true when there is a token and it has not expired', () => {
     global.localStorage = { getItem: jest.fn(key => ({ token: '', expiry: new Date(Date.now() + 1000).toISOString() })[key]) };
-    const user = getUser(null, emptyHashLocation);
+    const user = getUser(null, emptyHashWindow);
     expect(user.isAuthenticated()).toEqual(true);
   });
 
   describe('location has a hash', () => {
     let isAuthenticated;
-    const history = { pushState: jest.fn() };
+    const window = {
+      location: {
+        hash: '#access_token=FAKE&token_type=Bearer&expires_in=3600&state=reactApp',
+      },
+      history: {
+        pushState: jest.fn(),
+      },
+    };
     beforeAll(() => {
       global.localStorage = {
         getItem: jest.fn(key => ({ token: '', expiry: new Date(Date.now() + 1000).toISOString() })[key]),
         setItem: jest.fn(),
       };
-      const hash = '#access_token=FAKE&token_type=Bearer&expires_in=3600&state=reactApp';
-      const user = getUser(null, { hash }, history);
+      const user = getUser(null, window);
       isAuthenticated = user.isAuthenticated();
     });
 
@@ -53,7 +61,7 @@ describe('Auth module', () => {
     });
 
     it('pushes history state that clears the hash', () => {
-      expect(history.pushState).toHaveBeenCalled();
+      expect(window.history.pushState).toHaveBeenCalled();
     });
   });
 
@@ -61,13 +69,13 @@ describe('Auth module', () => {
     process.env.REACT_APP_SPOTIFY_CLIENT_ID = 'clientId';
     process.env.REACT_APP_SPOTIFY_AUTHORIZE_URL = 'http://auth.com';
     process.env.REACT_APP_SPOTIFY_SCOPES = 'theScopes';
-    const user = getUser(null, emptyHashLocation);
+    const user = getUser(null, emptyHashWindow);
     expect(user.getAuthUrl()).toEqual('http://auth.com?client_id=clientId&response_type=token&redirect_uri=http://frontend.org&state=reactApp&scope=theScopes&show_dialog=false');
   });
 
   it('constructs an api object', () => {
     const ApiClass = jest.fn(() => ({ setAccessToken: jest.fn() }));
-    const user = getUser(ApiClass, emptyHashLocation);
+    const user = getUser(ApiClass, emptyHashWindow);
     user.getApi();
     expect(ApiClass.mock.calls).toEqual([[{
       redirectUri: 'http://frontend.org',
