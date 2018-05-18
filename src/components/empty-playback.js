e14579ab64d6115fc958b27c63c2c9debe2c74ae
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-17 21:39:25 -0400

    Add empty playback component

---
+++ src/components/empty-playback.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const EmptyPlayback = () => (
+  <div>
+    {"You're not playing anything. Please start playing a song and then refresh the page."}
+  </div>
+);
+
+export default EmptyPlayback;
