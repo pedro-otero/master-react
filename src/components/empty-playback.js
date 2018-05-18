422474e14ca95c5af9a972fe73cac5c7d7dcd6aa
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-18 11:23:44 -0400

    Write a detailed message

--- src/components/empty-playback.js
+++ src/components/empty-playback.js
@@ -1,8 +1,12 @@
 import * as React from 'react';
 
+import './empty-playback.css';
+
 const EmptyPlayback = () => (
-  <div>
-    {"You're not playing anything. Please start playing a song and then refresh the page."}
+  <div className="emptyPlaybackMessage">
+    <p>Nothing found</p>
+    <p>{"When asked for the song you're currently listening to, Spotify said it's got nothing!"}</p>
+    <p>Please start playing a song and then refresh the page.</p>
   </div>
 );
 
