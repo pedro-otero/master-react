e05e38a9c679d03d8e25988cc2389579fa3a2894
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-18 11:32:36 -0400

    Add emoji

--- src/components/empty-playback.js
+++ src/components/empty-playback.js
@@ -4,6 +4,9 @@ import './empty-playback.css';
 
 const EmptyPlayback = () => (
   <div className="emptyPlaybackMessage">
+    <div>
+      <i className="em-svg em-thinking_face"></i>
+    </div>
     <p>Nothing found</p>
     <p>{"When asked for the song you're currently listening to, Spotify said it's got nothing!"}</p>
     <p>Please start playing a song and then refresh the page.</p>
