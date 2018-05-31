028431cedbea340f03ca9dc816c24a711b11d3bb
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-31 19:04:54 -0400

    Use named export

--- src/containers/App.spec.js
+++ src/containers/App.spec.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as Rx from 'rxjs';
 
-import App from './App';
+import { App } from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
