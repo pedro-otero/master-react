cc6282c6af9a7333ec1abfd8b833eaf10b72cd9c
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-31 18:10:17 -0400

    Provide store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import SpotifyWebApi from 'spotify-web-api-node';
 import request from 'superagent';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './redux/store';
 import App from './containers/App';
 
 import getBackend from './api/backend';
@@ -13,13 +15,16 @@ import getUser from './user';
 const Backend = getBackend(request, `${process.env.REACT_APP_BE_DOMAIN}/data/album`, 1000);
 const backend = new Backend();
 const user = getUser(SpotifyWebApi, window);
+const store = configureStore();
 
 registerServiceWorker();
 if (user.isAuthenticated()) {
   ReactDOM.render(
-    <App
-        spotifyApi={user.getApi()}
-        backend={backend} />,
+    <Provider store={store}>
+      <App
+          spotifyApi={user.getApi()}
+          backend={backend} />
+    </Provider>,
     document.getElementById('root'),
   );
 } else {
