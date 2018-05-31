2c6117b341c5c0e938e379bb41246771b4f528c5
Pedro Otero <pedro.otero.prada@gmail.com>
2018-05-31 18:10:06 -0400

    Export function to create store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import bestMatches from '../reducers/best-matches';
 
-const store = createStore(combineReducers({
+const store = () => createStore(combineReducers({
   bestMatches,
 }));
 
