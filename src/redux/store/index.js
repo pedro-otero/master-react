ebc6e79a9972f6aa44830a6f08e8f6dc45114c52
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-02 19:27:41 -0400

    Apply thunk middleware

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,16 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import generateReducer from '../reducers/generate-reducer';
 import setPlaybackInfo from '../reducers/playback-info';
 
-const store = () => createStore(combineReducers({
-  searches: generateReducer('SET_SEARCH_RESULT'),
-  albums: generateReducer('SET_ALBUM'),
-  artists: generateReducer('SET_ARTIST'),
-  playbackInfo: setPlaybackInfo,
-}));
+const store = () => createStore(
+  combineReducers({
+    searches: generateReducer('SET_SEARCH_RESULT'),
+    albums: generateReducer('SET_ALBUM'),
+    artists: generateReducer('SET_ARTIST'),
+    playbackInfo: setPlaybackInfo,
+  }),
+  applyMiddleware(thunkMiddleware),
+);
 
 export default store;
