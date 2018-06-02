97c5631243bbd04596965c0999455d2068764fc0
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-02 17:23:49 -0400

    Put reducer in store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import generateReducer from '../reducers/generate-reducer';
+import setPlaybackInfo from '../reducers/playback-info';
 
 const store = () => createStore(combineReducers({
   searches: generateReducer('SET_SEARCH_RESULT'),
   albums: generateReducer('SET_ALBUM'),
   artists: generateReducer('SET_ARTIST'),
+  playbackInfo: setPlaybackInfo,
 }));
 
 export default store;
