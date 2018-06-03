a96daac64cb631596454954271b540817f45c2a0
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-02 20:19:48 -0400

    Create load playback info action

--- src/redux/actions/playback-info.js
+++ src/redux/actions/playback-info.js
@@ -2,3 +2,9 @@ export const setPlaybackInfo = data => ({
   type: 'SET_PLAYBACK_INFO',
   data,
 });
+
+export const loadPlaybackInfo = () => (dispatch, getState, spotifyApi) => {
+  spotifyApi.getCurrentPlayback().then(({ body }) => {
+    dispatch(setPlaybackInfo(body));
+  });
+};
--- src/redux/actions/playback-info.spec.js
+++ src/redux/actions/playback-info.spec.js
@@ -1,4 +1,4 @@
-import { setPlaybackInfo } from './playback-info';
+import { setPlaybackInfo, loadPlaybackInfo } from './playback-info';
 
 describe('Playback info action creator', () => {
   it('creates SET_PLAYBACK_INFO action', () => {
@@ -7,5 +7,18 @@ describe('Playback info action creator', () => {
       type: 'SET_PLAYBACK_INFO',
       data: 'val',
     });
+  });
+
+  it('Loads playback info', (done) => {
+    const thunk = loadPlaybackInfo();
+    const api = {
+      getCurrentPlayback: jest.fn(() => Promise.resolve({})),
+    };
+    const dispatch = jest.fn((action) => {
+      expect(action.type).toEqual('SET_PLAYBACK_INFO');
+      expect(api.getCurrentPlayback).toHaveBeenCalled();
+      done();
+    });
+    thunk(dispatch, null, api);
   });
 });
