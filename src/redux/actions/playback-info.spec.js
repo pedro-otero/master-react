35c1dbebed95bc22aa6634b233d91f393fb1279b
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-02 20:06:48 -0400

    Rename

--- src/redux/actions/playback-info.spec.js
+++ src/redux/actions/playback-info.spec.js
@@ -1,4 +1,4 @@
-import { setPlaybackInfo } from './set-playback-info';
+import { setPlaybackInfo } from './playback-info';
 
 describe('Set playback info action creator', () => {
   it('creates action', () => {
