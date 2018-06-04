a804709724716f314c619cd620b0fd9ad296400e
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 19:44:58 -0400

    Refactor test

--- src/redux/actions/backend.spec.js
+++ src/redux/actions/backend.spec.js
@@ -3,31 +3,33 @@ import * as Rx from 'rxjs';
 import { loadSearchResult } from './backend';
 
 describe('Backend actions', () => {
-  const dispatch = jest.fn();
-  const actions = {
-    setSearchResult: jest.fn(),
-  };
-  const backend = {
-    getCredits: jest.fn(() => Rx.Observable.create((subscriber) => {
-      subscriber.next({});
-      subscriber.complete();
-    })),
-  };
+  describe('Successful search result', () => {
+    const dispatch = jest.fn();
+    const actions = {
+      setSearchResult: jest.fn(),
+    };
+    const backend = {
+      getCredits: jest.fn(() => Rx.Observable.create((subscriber) => {
+        subscriber.next({});
+        subscriber.complete();
+      })),
+    };
 
-  beforeAll((done) => {
-    const thunk = loadSearchResult('AL1');
-    thunk(dispatch, null, { backend, actions }).then(done);
-  });
+    beforeAll((done) => {
+      const thunk = loadSearchResult('AL1');
+      thunk(dispatch, null, { backend, actions }).then(done);
+    });
 
-  it('informs data is loading', () => {
-    expect(actions.setSearchResult).toHaveBeenCalledWith('AL1', 'LOADING');
-  });
+    it('informs data is loading', () => {
+      expect(actions.setSearchResult).toHaveBeenCalledWith('AL1', 'LOADING');
+    });
 
-  it('calls backend', () => {
-    expect(backend.getCredits).toHaveBeenCalledWith('AL1');
-  });
+    it('calls backend', () => {
+      expect(backend.getCredits).toHaveBeenCalledWith('AL1');
+    });
 
-  it('informs load finished', () => {
-    expect(actions.setSearchResult).toHaveBeenCalledWith('AL1', {});
+    it('informs load finished', () => {
+      expect(actions.setSearchResult).toHaveBeenCalledWith('AL1', {});
+    });
   });
 });
