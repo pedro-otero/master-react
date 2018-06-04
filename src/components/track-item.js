284a79c3f4dae2e6376ed5c575978774d7235776
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 15:11:07 -0400

    Render basics

--- src/components/track-item.js
+++ src/components/track-item.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const TrackItem = ({ number, fromSpotify, fromSearch }) => {
-  return <div>
+import './track-item.css';
+import Composers from './composers';
 
-  </div>;
-};
+const duration = millis => '0:01';
+
+const TrackItem = ({
+  number, fromSpotify: { name, duration_ms: millis }, fromSearch: { composers },
+}) => <div>
+  <div className="track-item-number">{number}</div>
+  <div className="track-item-name">{name}</div>
+  <div className="track-item-duration">{duration(millis)}</div>
+  <Composers list={composers} />
+</div>;
 
 TrackItem.propTypes = {
-  fromSearch: PropTypes.object,
-  fromSpotify: PropTypes.object,
-  number: PropTypes.number,
+  fromSearch: PropTypes.object.isRequired,
+  fromSpotify: PropTypes.object.isRequired,
+  number: PropTypes.number.isRequired,
 };
 
 export default TrackItem;
--- src/components/track-item.spec.js
+++ src/components/track-item.spec.js
@@ -7,7 +7,24 @@ import TrackItem from './track-item';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Track item component', () => {
-  it('renders OK', () => {
-    shallow(<TrackItem />);
+  const wrapper = shallow(<TrackItem
+      number={1}
+      fromSearch={{ composers: ['C1'] }}
+      fromSpotify={{ name: 'Track', duration_ms: 1000 }} />);
+
+  it('renders number', () => {
+    expect(wrapper.find('div[className="track-item-number"]').text()).toEqual('1');
+  });
+
+  it('renders name', () => {
+    expect(wrapper.find('div[className="track-item-name"]').text()).toEqual('Track');
+  });
+
+  it('renders composers', () => {
+    expect(wrapper.find('Composers').length).toEqual(1);
+  });
+
+  it('renders duration', () => {
+    expect(wrapper.find('div[className="track-item-duration"]').text()).toEqual('0:01');
   });
 });
