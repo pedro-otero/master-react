e8ee542b3548d59bc3c7b500366f446b2b0d6c2e
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 15:42:24 -0400

    Format time

--- src/components/track-item.js
+++ src/components/track-item.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 import './track-item.css';
 import Composers from './composers';
 
-const duration = millis => '0:01';
+const duration = (millis) => {
+  const time = new Date();
+  time.setMilliseconds(time.getTime() + millis);
+  return `${time.getMinutes()}:${time.getSeconds()}`;
+};
 
 const TrackItem = ({
   number, fromSpotify: { name, duration_ms: millis }, fromSearch: { composers },
 }) => <div className="track-item-div">
   <div>
     <div className="track-item-number">{number}</div>
-    <div className="track-item-name">{name}</div>
+    <div className="track-item-name">
+      <div>{name}</div>
+      <Composers list={composers} />
+    </div>
     <div className="track-item-duration">{duration(millis)}</div>
-  </div>
-  <div>
-    <div className="track-item-number"> </div>
-    <Composers list={composers} />
   </div>
 </div>;
 
