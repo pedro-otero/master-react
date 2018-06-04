0c8df31d258a6488891a34c781a53067511f46e6
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 15:01:08 -0400

    Add component

---
+++ src/components/track-item.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const TrackItem = ({ number, fromSpotify, fromSearch }) => {
+  return <div>
+
+  </div>;
+};
+
+TrackItem.propTypes = {
+  fromSearch: PropTypes.object,
+  fromSpotify: PropTypes.object,
+  number: PropTypes.number,
+};
+
+export default TrackItem;
