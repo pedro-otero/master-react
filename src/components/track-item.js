0599008ebcb44e3ef918f91864aad8ae100313d9
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 15:17:09 -0400

    Style items

--- src/components/track-item.js
+++ src/components/track-item.js
@@ -8,10 +8,12 @@ const duration = millis => '0:01';
 
 const TrackItem = ({
   number, fromSpotify: { name, duration_ms: millis }, fromSearch: { composers },
-}) => <div>
-  <div className="track-item-number">{number}</div>
-  <div className="track-item-name">{name}</div>
-  <div className="track-item-duration">{duration(millis)}</div>
+}) => <div className="track-item-div">
+  <div>
+    <div className="track-item-number">{number}</div>
+    <div className="track-item-name">{name}</div>
+    <div className="track-item-duration">{duration(millis)}</div>
+  </div>
   <Composers list={composers} />
 </div>;
 
