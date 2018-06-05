62de1411886b38a2ca98e764472c0b8be1ea363f
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-04 20:05:08 -0400

    Turn into ES6 class

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,24 +1,31 @@
 import * as Rx from 'rxjs';
 
-export default function Backend(request, url, pollFreq) {
-  this.getCredits = albumId => Rx.Observable.create((subscriber) => {
-    let timer;
-    const retrieve = receive => request.get(`${url}/${albumId}`).end(receive);
-    const receive = (err, res) => {
-      if (err) {
-        subscriber.error(err);
-      } else {
-        subscriber.next(res.body);
-        if (res.body.progress === 100) {
-          subscriber.complete();
+export default class Backend {
+  constructor(request, url, pollFreq) {
+    this.opts = { request, url, pollFreq };
+  }
+
+  getCredits(albumId) {
+    const { request, url, pollFreq } = this.opts;
+    return Rx.Observable.create((subscriber) => {
+      let timer;
+      const retrieve = receive => request.get(`${url}/${albumId}`).end(receive);
+      const receive = (err, res) => {
+        if (err) {
+          subscriber.error(err);
         } else {
-          timer = setTimeout(() => retrieve(receive), pollFreq);
+          subscriber.next(res.body);
+          if (res.body.progress === 100) {
+            subscriber.complete();
+          } else {
+            timer = setTimeout(() => retrieve(receive), pollFreq);
+          }
         }
-      }
-    };
-    retrieve(receive);
-    return () => {
-      clearTimeout(timer);
-    };
-  });
+      };
+      retrieve(receive);
+      return () => {
+        clearTimeout(timer);
+      };
+    });
+  }
 }
