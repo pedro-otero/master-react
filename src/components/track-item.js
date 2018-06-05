9627c081f084af5066652e327b28714870c4b45b
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-05 17:40:52 -0400

    Link to track

--- src/components/track-item.js
+++ src/components/track-item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import './track-item.css';
 import Composers from './composers';
@@ -11,16 +12,21 @@ function duration(millis) {
 }
 
 const TrackItem = ({
-  fromSpotify: { name, duration_ms: millis }, fromSearch: { composers },
-}) => <div className="track-item-div">
-  <div>
-    <div className="track-item-center-block">
-      <div className="track-item-name">{name}</div>
-      <Composers list={composers} />
+  fromSpotify: { name, duration_ms: millis, id }, fromSearch: { composers },
+}) =>
+  <Link
+      to={`/track/${id}`}
+      className="RR-link">
+    <div className="track-item-div">
+      <div>
+        <div className="track-item-center-block">
+          <div className="track-item-name">{name}</div>
+          <Composers list={composers} />
+        </div>
+        <div className="track-item-duration">{duration(millis)}</div>
+      </div>
     </div>
-    <div className="track-item-duration">{duration(millis)}</div>
-  </div>
-</div>;
+  </Link>;
 
 TrackItem.propTypes = {
   fromSearch: PropTypes.object.isRequired,
