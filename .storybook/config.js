8e64617965f47d227311b6a6fdd865f852935fb0
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-06 11:56:43 -0400

    Use own styles and move body tag to global styles file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies,global-require */
 import { configure } from '@storybook/react';
 
+import '../src/index.css';
+
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
