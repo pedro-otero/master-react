f46b637e8296867e43039f98eff18cb39bb21af7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-06 11:35:24 -0400

    Add storybook support and sample story with TrackItem

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies,global-require */
+import { configure } from '@storybook/react';
+
+function loadStories() {
+  require('../stories/index.js');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
---
+++ stories/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import TrackItem from '../src/components/track-item';
+
+storiesOf('Track item', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('With composers', () => (
+    <TrackItem
+        fromSearch={{
+      composers: ['The one', 'The other'],
+    }}
+        fromSpotify={{
+    name: 'Hot New Track',
+    duration_ms: 25693579,
+  }} />
+  ));
