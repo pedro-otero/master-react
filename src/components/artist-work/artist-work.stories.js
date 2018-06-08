5a4178a109d680ff27237c6ba1177f10923a93cd
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-08 14:56:09 -0400

    Add stories

---
+++ src/components/artist-work/artist-work.stories.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { storiesOf } from '@storybook/react';
+
+import ArtistWork from './artist-work';
+
+storiesOf('Artist Work', module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .add('Default', () => (
+    <ArtistWork
+        artist="The Band"
+        title="Something They Did"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2018" />
+  ));
