a2900791302fca426ab873ed47f376ce4e66f432
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-09 15:44:25 -0400

    Change message

--- src/components/empty-playback/empty-playback.js
+++ src/components/empty-playback/empty-playback.js
@@ -10,7 +10,7 @@ const EmptyPlayback = () => (
     </div>
     <p>Nothing found</p>
     <p>{"When asked for the song you're currently listening to, Spotify said it's got nothing!"}</p>
-    <p>Please start playing a song and then refresh the page.</p>
+    <p>Start playing a song, dust it off and...</p>
     <Link
         to="/"
         className="RR-link">
