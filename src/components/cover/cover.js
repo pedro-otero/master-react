698ccb45f67aadc51ccdccf5e81a48061ff3afc8
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-12 15:52:38 -0400

    Move all styles in stylesheets to the cover styled component

--- src/components/cover/cover.js
+++ src/components/cover/cover.js
@@ -7,6 +7,48 @@ import './cover.css';
 
 const Image = styled.div`
   background-image: url(${props => props.src});
+  background-size: cover;
+  position: relative;
+ 
+  @media (max-width: 320px) {
+    margin-right: 0.5em;
+    min-width: 6em;
+    max-width: 6em;
+    min-height: 6em;
+    max-height: 6em;
+  }
+
+  @media (min-width: 321px) and (max-width: 768px) {
+    margin-right: 0.5em;
+    min-width: 7em;
+    max-width: 7em;
+    min-height: 7em;
+    max-height: 7em;
+  }
+
+  @media (min-width: 769px) and (max-width: 1280px) {
+    margin-right: 1.7em;
+    min-width: 8em;
+    max-width: 8em;
+    min-height: 8em;
+    max-height: 8em;
+  }
+  
+  @media (min-width: 1281px) and (max-width: 1919px) {
+    margin-right: 2em;
+    min-width: 10em;
+    max-width: 10em;
+    min-height: 10em;
+    max-height: 10em;
+  }
+  
+  @media (min-width: 1920px) {
+    margin-right: 2em;
+    min-width: 12em;
+    max-width: 12em;
+    min-height: 12em;
+    max-height: 12em;
+  }
 `;
 
 const Cover = ({
