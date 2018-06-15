12c9a736e9e98849b7dafcc0035c1152ffce8bc9
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-15 18:43:24 -0400

    Remove outdated comments

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -134,11 +134,6 @@ module.exports = {
               cacheDirectory: true,
             },
           },
-          // "postcss" loader applies autoprefixer to our CSS.
-          // "css" loader resolves paths in CSS and adds assets as dependencies.
-          // "style" loader turns CSS into JS modules that inject <style> tags.
-          // In production, we use a plugin to extract that CSS to a file, but
-          // in development "style" loader enables hot editing of CSS.
           {
             test: /\.css$/,
             use: [
