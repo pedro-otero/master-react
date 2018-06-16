510ca93729dee150fbdbc90cfe6580c4c198938a
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-16 09:20:26 -0400

    Remove unused import

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -8,7 +8,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
