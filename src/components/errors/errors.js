5b57567df57ed51f981781d541e932207b106bf7
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-19 16:37:25 -0400

    Add errors component

---
+++ src/components/errors/errors.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Error = styled.div`
+  background-color: red;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+const Errors = ({ list }) => (
+  <Fragment>
+    {list.map(error => <Error key={`error-${error}`}>{error}</Error>)}
+  </Fragment>
+);
+
+Errors.propTypes = {
+  list: PropTypes.array,
+};
+
+export default Errors;
