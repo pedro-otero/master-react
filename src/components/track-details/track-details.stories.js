8ac5ae66ea4f9a00de9e7c659f27a6b8bf8d2b78
Pedro Otero <pedro.otero.prada@gmail.com>
2018-06-30 23:55:39 -0400

    Add stories

--- src/components/track-details/track-details.stories.js
+++ src/components/track-details/track-details.stories.js
@@ -9,6 +9,68 @@ storiesOf('Track details', module)
   .addDecorator(story => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
+  .add('Just started', () => (
+    <TrackDetails />
+  ))
+  .add('With Spotify track info', () => (
+    <TrackDetails name="Char Topper Everybody's Sick Of" />
+  ))
+  .add('With Spotify album info', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        year="2016" />
+  ))
+  .add('With Spotify artist info', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        artist="Pop Master"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2016" />
+  ))
+  .add('Search started', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        artist="Pop Master"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2016"
+        progress={0} />
+  ))
+  .add('Search progressing', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        artist="Pop Master"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2016"
+        progress={15} />
+  ))
+  .add('Found composers', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        artist="Pop Master"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2016"
+        composers={['Realname Ofpopmaster', 'Goto Producer']}
+        progress={21} />
+  ))
+  .add('Found credits', () => (
+    <TrackDetails
+        name="Char Topper Everybody's Sick Of"
+        artist="Pop Master"
+        image="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        background="https://i.scdn.co/image/02bd189433691a8eb843f7bc3a82d8355938469a"
+        year="2016"
+        composers={['Realname Ofpopmaster', 'Goto Producer']}
+        credits={{
+          'Goto Producer': ['Backing Vocals'],
+          'Guy Bass': ['Bass'],
+        }}
+        progress={41} />
+  ))
   .add('Finished', () => (
     <TrackDetails
         name="Char Topper Everybody's Sick Of"
