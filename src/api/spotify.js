503454a90bf766d0db3c9e9bd1d2a80e01205c38
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-02 18:46:26 -0400

    Refactor

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -1,32 +1,29 @@
 export default (SpotifyWebApi, location) => ({ clientId, redirectUri, throttle }) => {
   const commands = [];
+  const methods = ['getMyCurrentPlaybackState', 'getAlbum', 'getArtist', 'getTrack'];
   const api = new SpotifyWebApi({ clientId, redirectUri });
   api.setAccessToken(localStorage.getItem('token'));
+
+  const processRejection = reject => (e) => {
+    const { statusCode } = e;
+    if (statusCode === 401) {
+      location.reload();
+      return;
+    }
+    reject(e);
+  };
 
   setInterval(() => {
     if (commands.length) {
       const [
         method, args, resolve, reject,
       ] = commands.shift();
-      api[method](...args).then(resolve, (e) => {
-        const { statusCode } = e;
-        if (statusCode === 401) {
-          location.reload();
-          return;
-        }
-        reject(e);
-      });
+      api[method](...args).then(resolve, processRejection(reject));
     }
   }, throttle);
 
-  const pushCommand = (method, args) => new Promise((resolve, reject) =>
-    commands.push([method, args, resolve, reject]));
-
-  const methods = ['getMyCurrentPlaybackState', 'getAlbum', 'getArtist', 'getTrack'];
-
   return methods.reduce((publicApi, method) => Object.assign({}, publicApi, {
-    [method](...args) {
-      return pushCommand(method, args);
-    },
+    [method]: (...args) => new Promise((resolve, reject) =>
+      commands.push([method, args, resolve, reject])),
   }), {});
 };
