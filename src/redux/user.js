36080a08be6c6a4aab1dc4c12438592573c070ed
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-02 14:17:57 -0400

    Add set token action

---
+++ src/redux/user.js
@@ -0,0 +1,17 @@
+export function setToken(hash) {
+  const pairs = hash.substring(1).split('&').map(pair => pair.split('='));
+  const token = pairs.filter(pair => pair[0] === 'access_token')[0][1];
+  const expiresIn = pairs.filter(pair => pair[0] === 'expires_in')[0][1];
+  const expiry = new Date(Date.now() + (expiresIn * 1000)).toISOString();
+  return { type: 'SET_TOKEN', data: { token, expiry } };
+}
+
+export function reduce(state = {}, { type, data }) {
+  switch (type) {
+    case 'SET_TOKEN': {
+      const { token, expiry } = data;
+      return Object.assign({}, state, { token, expiry });
+    }
+  }
+  return state;
+}
---
+++ src/redux/user.spec.js
@@ -0,0 +1,8 @@
+import { setToken } from './user';
+
+describe('REDUX: User', () => {
+  it('creates SET_TOKEN action', () => {
+    const { type, data: { token } } = setToken('#access_token=FAKE&token_type=Bearer&expires_in=3600&state=reactApp');
+    expect(type === 'SET_TOKEN' && token === 'FAKE');
+  });
+});
