b1968e442134de8e05e000ffb3b4cb93549572a2
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-02 20:47:37 -0400

    Change name of action to parseToken

--- src/redux/user.js
+++ src/redux/user.js
@@ -1,12 +1,13 @@
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
+export const PARSE_TOKEN = 'PARSE_TOKEN';
 
-export function setToken(hash) {
+export function parseToken(hash) {
   const pairs = hash.substring(1).split('&').map(pair => pair.split('='));
   const token = pairs.filter(pair => pair[0] === 'access_token')[0][1];
   const expiresIn = pairs.filter(pair => pair[0] === 'expires_in')[0][1];
   const expiry = new Date(Date.now() + (expiresIn * 1000)).toISOString();
-  return { type: SET_TOKEN, data: { token, expiry } };
+  return { type: PARSE_TOKEN, data: { token, expiry } };
 }
 
 export function logout() {
@@ -15,7 +16,7 @@ export function logout() {
 
 export function reduce(state = {}, { type, data }) {
   switch (type) {
-    case SET_TOKEN: {
+    case PARSE_TOKEN: {
       const { token, expiry } = data;
       return Object.assign({}, state, { token, expiry });
     }
--- src/redux/user.spec.js
+++ src/redux/user.spec.js
@@ -1,8 +1,8 @@
-import { SET_TOKEN, setToken } from './user';
+import { PARSE_TOKEN, parseToken } from './user';
 
 describe('REDUX: User', () => {
-  it('creates SET_TOKEN action', () => {
-    const { type, data: { token } } = setToken('#access_token=FAKE&token_type=Bearer&expires_in=3600&state=reactApp');
-    expect(type === SET_TOKEN && token === 'FAKE');
+  it('creates PARSE_TOKEN action', () => {
+    const { type, data: { token } } = parseToken('#access_token=FAKE&token_type=Bearer&expires_in=3600&state=reactApp');
+    expect(type === PARSE_TOKEN && token === 'FAKE');
   });
 });
