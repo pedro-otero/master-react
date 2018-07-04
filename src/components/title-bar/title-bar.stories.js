2ebd5557e7550a257ca58b7ea982fbd2ef103265
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-04 07:20:26 -0400

    Lint

--- src/components/title-bar/title-bar.stories.js
+++ src/components/title-bar/title-bar.stories.js
@@ -7,6 +7,6 @@ import TitleBar from './title-bar';
 storiesOf('Title Bar', module)
   .add('Default', () => (
     <TitleBar
-      avatar="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
-      title="User name" />
+        avatar="https://i.scdn.co/image/edb1577fa1a7b3e9e0f07297071cf6076a1946c3"
+        title="User name" />
   ));
