45b827ce44acb3ab3ae00b6b0e48be053e6751c6
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-04 09:36:37 -0400

    Change property name

--- src/redux/profile.js
+++ src/redux/profile.js
@@ -6,7 +6,7 @@ export function setProfile({ display_name: name, id, images }) {
   return {
     type: SET_PROFILE,
     data: {
-      name, id, image: images.length ? images[0].url : null,
+      name, id, avatar: images.length ? images[0].url : null,
     },
   };
 }
