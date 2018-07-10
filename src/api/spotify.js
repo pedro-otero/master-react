1dd7ed4eda817215517d39ecd757d47a41b8582d
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-10 19:24:36 -0400

    Remove command before it's started so subsequent queue shifts don't run the same command. Add it back if needed.

--- src/api/spotify.js
+++ src/api/spotify.js
@@ -5,30 +5,26 @@ export default (SpotifyWebApi, location) => ({ clientId, redirectUri, throttle }
   const api = new SpotifyWebApi({ clientId, redirectUri });
   api.setAccessToken(localStorage.getItem('token'));
 
-  const processRejection = reject => (e) => {
-    const { statusCode } = e;
-    if (statusCode === 401) {
-      location.reload();
-      return;
-    } else if (statusCode === 429) {
-      clearInterval(timer);
-      // This fixed 500 value HAS TO BE CHANGED for the one in the Retry-After header
-      // that is not exposed by the Spotify Web API wrapper.
-      // See issue https://github.com/thelinmichael/spotify-web-api-node/issues/217
-      setTimeout(start, 500);
-      return;
-    }
-    reject(e);
-  };
-
   function start() {
     timer = setInterval(() => {
       if (commands.length) {
-        const [method, args, resolve, reject] = commands[0];
-        api[method](...args).then((response) => {
-          commands.shift();
-          resolve(response);
-        }, processRejection(reject));
+        const [method, args, resolve, reject] = commands.shift();
+        api[method](...args).then(resolve, (e) => {
+          const { statusCode } = e;
+          if (statusCode === 401) {
+            location.reload();
+            return;
+          } else if (statusCode === 429) {
+            commands.unshift([method, args, resolve, reject]);
+            clearInterval(timer);
+            // This fixed 500 value HAS TO BE CHANGED for the one in the Retry-After header
+            // that is not exposed by the Spotify Web API wrapper.
+            // See issue https://github.com/thelinmichael/spotify-web-api-node/issues/217
+            setTimeout(start, 500);
+            return;
+          }
+          reject(e);
+        });
       }
     }, throttle);
   }
