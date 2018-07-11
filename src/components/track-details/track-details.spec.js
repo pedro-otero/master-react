1ada5dfaa603e80c9480edf10e9cfffa21033462
Pedro Otero <pedro.otero.prada@gmail.com>
2018-07-11 15:39:44 -0400

    Fix import

--- src/components/track-details/track-details.spec.js
+++ src/components/track-details/track-details.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import { TrackDetails } from './track-details';
+import TrackDetails from './track-details';
 
 const credits = { composers: [], producers: [], credits: {} };
 
