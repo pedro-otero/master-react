eea8444854c4199ca8bf3a10b0fbc55fc33dfb68
Pedro Otero <pedro.otero.prada@gmail.com>
2018-09-01 21:38:00 -0400

    Use link

--- src/components/track-item/track-item.js
+++ src/components/track-item/track-item.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
+import Link from 'components/Link';
 import Composers from 'components/Composers';
 import styles from './track-item.css';
-import globalStyles from '../../index.css';
 
 const TrackItem = ({
   name, duration, id, composers,
 }) =>
-  <Link
-      to={`/track/${id}`}
-      className={globalStyles.RRlink}>
+  <Link to={`/track/${id}`}>
     <div className={styles.div}>
       <div className={styles.centerBlock}>
         <div className={styles.name}>{name}</div>
