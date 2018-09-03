085f9138e09e691baf0d6cacc37cc6a06d894aef
Pedro Otero <pedro.otero.prada@gmail.com>
2018-09-02 22:41:21 -0400

    Move style

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -2,12 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
+import styled from 'styled-components';
 
 import globalStyles from '../../index.css';
 
+const StrippedDownLink = styled(Link)`
+  all: unset;
+  cursor: pointer;
+`;
+
 const StyledLink = ({ className, ...otherProps }) => {
   const classes = classnames(globalStyles.RRlink, className);
-  return <Link className={classes} {...otherProps} />;
+  return <StrippedDownLink className={classes} {...otherProps} />;
 };
 
 StyledLink.displayName = 'Link';
