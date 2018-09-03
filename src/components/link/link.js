525476d80abe16628a7849d60ad2517975652719
Pedro Otero <pedro.otero.prada@gmail.com>
2018-09-02 22:22:20 -0400

    Pass through className

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import globalStyles from '../../index.css';
 
-const StyledLink = props => (
-  <Link className={globalStyles.RRlink} {...props} />
-);
+const StyledLink = ({ className, ...otherProps }) => {
+  const classes = classnames(globalStyles.RRlink, className);
+  return <Link className={classes} {...otherProps} />;
+};
 
 StyledLink.displayName = 'Link';
+
+StyledLink.propTypes = {
+  className: PropTypes.any,
+};
 
 export default StyledLink;
--- src/components/link/link.spec.js
+++ src/components/link/link.spec.js
@@ -7,8 +7,8 @@ import Link from './link';
 describe('Link component', () => {
   it('passes through props to react router Link', () => {
     const wrapper = shallow(<MemoryRouter initialEntries={['/']}>
-      <Link to="somewhere" />
-    </MemoryRouter>).dive();
-    expect(wrapper.find('Link[to="somewhere"]').length).toEqual(1);
+      <Link to="somewhere" className="anotherClass" />
+    </MemoryRouter>).dive().dive();
+    expect(wrapper.find('.anotherClass').length).toEqual(1);
   });
 });
