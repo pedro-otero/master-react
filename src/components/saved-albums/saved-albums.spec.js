61d3802e699fff29c2ff4f5d16c16981806c52b6
Pedro Otero <pedro.otero.prada@gmail.com>
2018-09-09 16:12:29 -0400

    Add saved albums component

---
+++ src/components/saved-albums/saved-albums.spec.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { SavedAlbums, mapStateToProps } from './saved-albums';
+
+describe('Saved albums', () => {
+  describe('Component', () => {
+    it('fetches albums on load', () => {
+      const mock = jest.fn();
+      shallow(<SavedAlbums loadSavedAlbums={mock} albums={[]} />);
+      expect(mock).toBeCalled();
+    });
+  });
+
+  describe('mapStateToProps', () => {
+    it('gets all the data for an album', () => {
+      const state = {
+        user: {
+          library: {
+            albums: {
+              items: {
+                L1: {
+                  id: 'L1', name: 'Album', artist: 'Artist',
+                },
+              },
+            },
+          },
+        },
+      };
+      const props = mapStateToProps(state);
+      expect(props).toEqual({
+        albums: [{
+          id: 'L1',
+          name: 'Album',
+          artist: 'Artist',
+        }],
+      });
+    });
+  });
+});
