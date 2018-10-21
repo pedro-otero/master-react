ea9948ea1c0a0994e59fe3bdf90e3e387519497b
Pedro Otero <pedro.otero.prada@gmail.com>
2018-10-20 22:07:23 -0400

    Remove unused helper

--- src/redux/base/helpers.js
+++ src/redux/base/helpers.js
@@ -1,11 +1,3 @@
-export const updateState = (state, defaultItem) => tracks => tracks.reduce((all, track) => {
-  const merged = Object.assign(
-    { ...(all[track.id] || defaultItem) },
-    track.value,
-  );
-  return Object.assign({ ...all }, { [track.id]: merged });
-}, state);
-
 export const loadThunk = (id, items, dispatch, start, load, set, fail) => {
   const item = items[id];
   if (!item || item.failed) {
