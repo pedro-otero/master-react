c8717ed79de4a0fcdc4a563da341b7a72cd31f74
Pedro Otero <pedro.otero.prada@gmail.com>
2018-11-07 22:26:08 -0500

    Import status reducer

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -5,13 +5,26 @@ import thunkMiddleware from 'redux-thunk';
 import { reduce as playbackInfo, playbackInfoActions } from '../playbackInfo';
 import { loadSearchResult, setSearchResult } from '../actions/backend';
 import { setArtist, startArtistLoad, loadArtist, failArtistLoad, reduce as artists } from '../artists';
-import { loadAlbum, startAlbumLoad, failAlbumLoad, setAlbum, reduce as albums } from '../albums';
-import { loadTrack, setTrack, startTrackLoad, failTrackLoad, reduce as tracks } from '../tracks';
+import {
+  loadAlbum,
+  startAlbumLoad,
+  failAlbumLoad,
+  setAlbum,
+  reduce as albums,
+} from '../albums';
+import {
+  loadTrack,
+  setTrack,
+  startTrackLoad,
+  failTrackLoad,
+  reduce as tracks,
+} from '../tracks';
 import { reduce as auth } from '../user';
 import { reduce as profile, userProfileActions } from '../profile';
 import { addError, clearErrors, reduce } from '../errors';
 import { savedTracksReducer, setSavedTracks, savedAlbumsReducer, setSavedAlbums } from '../library';
 import { viewTrack } from '../view';
+import status from 'state/status';
 
 const devTools = global.window.__REDUX_DEVTOOLS_EXTENSION__ &&
   global.window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -41,6 +54,7 @@ const store = (spotifyApi, backend) => createStore(
       }),
     }),
     errors: reduce,
+    status,
   }),
   devTools,
   applyMiddleware(thunkMiddleware.withExtraArgument({
