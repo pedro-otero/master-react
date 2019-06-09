5a1684fd906abbecef6451ecba630d138e2a0fc8
Pedro Otero <pedro.otero.prada@gmail.com>
2019-06-09 19:16:03 -0400

    Refactor TrackDetails tests

--- src/components/track-details/track-details.spec.js
+++ src/components/track-details/track-details.spec.js
@@ -3,60 +3,57 @@ import { shallow } from 'enzyme';
 
 import { TrackDetails } from './track-details';
 
-const credits = { composers: [], producers: [], credits: {} };
-
 describe('TrackDetails component', () => {
-  let wrapper;
-  const clearAlbumInView = jest.fn();
-  beforeEach(() => {
-    wrapper = shallow(<TrackDetails clearAlbumInView={clearAlbumInView} />);
-  });
-
   it('hides composers and producers list', () => {
+    const wrapper = shallow(<TrackDetails />);
+
     expect(wrapper.find('JointList')).toHaveLength(0);
   });
 
   it('displays the banner', () => {
-    wrapper.setProps({
-      name: 'Track name',
-      background: 'ImgUrl',
-    });
+    const wrapper = shallow(<TrackDetails
+        name="Track name"
+        background="ImgUrl" />);
+
     expect(wrapper.find('ArtistWork').prop('title')).toEqual('Track name');
   });
 
   it('does not display small progress indicator', () => {
-    wrapper.setProps({
-      name: 'Track',
-      progress: 0,
-    });
+    const wrapper = shallow(<TrackDetails
+        name="Track name"
+        progress={0} />);
+
     expect(wrapper.find('Progress[size="small"]')).toHaveLength(0);
   });
 
   it('does not display big progress indicator', () => {
-    wrapper.setProps({
-      name: 'Track',
-      progress: 10,
-    });
+    const wrapper = shallow(<TrackDetails
+        name="Track name"
+        progress={10} />);
+
     expect(wrapper.find('Progress[size="big"]')).toHaveLength(0);
   });
 
   it('does not display big progress indicator', () => {
-    wrapper.setProps({
-      name: 'Track',
-      progress: 100,
-    });
+    const wrapper = shallow(<TrackDetails
+        name="Track name"
+        progress={100} />);
+
     expect(wrapper.find('Progress[className="big-progress"]')).toHaveLength(0);
   });
 
   it('does not display small progress indicator', () => {
-    wrapper.setProps({
-      name: 'Track',
-      progress: 100,
-    });
+    const wrapper = shallow(<TrackDetails
+        name="Track name"
+        progress={100} />);
+
     expect(wrapper.find('Progress[className="small-progress"]')).toHaveLength(0);
   });
 
   it('clears the album in view', () => {
+    const clearAlbumInView = jest.fn();
+    const wrapper = shallow(<TrackDetails clearAlbumInView={clearAlbumInView} />);
+
     wrapper.unmount();
 
     expect(clearAlbumInView).toHaveBeenCalled();
