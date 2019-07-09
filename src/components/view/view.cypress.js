b5bc0b6d4e967155401d17c0f053e21a2e67e733
Pedro Otero <pedro.otero.prada@gmail.com>
2019-07-08 20:53:53 -0400

    Remove View component

--- src/components/view/view.cypress.js
+++
@@ -1,9 +0,0 @@
-describe('View', () => {
-  it('Stories', () => {
-    cy.visit('http://localhost:9001/iframe.html?id=view--failed');
-    cy.contains('And this one is the failedMessage prop');
-
-    cy.visit('http://localhost:9001/iframe.html?id=view--all-ok');
-    cy.contains('View is a component that displays any full view in the app');
-  });
-});
